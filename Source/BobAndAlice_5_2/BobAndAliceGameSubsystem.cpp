2f1f2b610979608a3a6dc8db624191edc4490505
Uryume <94563834+Uryume@users.noreply.github.com>
2023-12-14 22:28:46 -0500

    commit

---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
@@ -0,0 +1,36 @@
+﻿#include "BobAndAliceGameSubsystem.h"
+
+void UBobAndAliceGameSubsystem::Initialize(
+    FSubsystemCollectionBase &Collection) {
+  Super::Initialize(Collection);
+}
+
+void UBobAndAliceGameSubsystem::Deinitialize() { Super::Deinitialize(); }
+
+void UBobAndAliceGameSubsystem::SaveTimerToSubSystem(const float TimeLimit)
+{
+	LocalTimer = TimeLimit;
+}
+float UBobAndAliceGameSubsystem::GetLocalSavedTimer() const
+{
+	return LocalTimer;
+}
+
+void UBobAndAliceGameSubsystem::SavePackageCollected(const bool PackageCollected)
+{
+	isPackageCollected = PackageCollected;
+}
+bool UBobAndAliceGameSubsystem::GetPackageCollected() const
+{
+	return isPackageCollected;
+}
+
+void UBobAndAliceGameSubsystem::SavePackageDelivered(const bool Delivered)
+{
+	LocalPackagedDelivered = Delivered;
+}
+
+bool UBobAndAliceGameSubsystem::GetPackagedDelivered() const
+{
+	return LocalPackagedDelivered;
+}
---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "BobAndAliceGameSubsystem.generated.h"
+
+UCLASS()
+class BOBANDALICE_5_2_API UBobAndAliceGameSubsystem : public UGameInstanceSubsystem
+{
+  GENERATED_BODY()
+
+public:
+  virtual void Initialize(FSubsystemCollectionBase &Collection) override;
+
+  virtual void Deinitialize() override;
+
+  UFUNCTION(BlueprintCallable)
+  void SaveTimerToSubSystem(const float TimeLimit);
+  UFUNCTION(BlueprintCallable,BlueprintPure)
+  float GetLocalSavedTimer() const;
+
+  UFUNCTION(BlueprintCallable)
+  void SavePackageDelivered(const bool Delivered);
+  UFUNCTION(BlueprintCallable,BlueprintPure)
+  bool GetPackagedDelivered() const;
+  
+  UFUNCTION(BlueprintCallable)
+  void SavePackageCollected(const bool PackageCollected);
+  UFUNCTION(BlueprintCallable,BlueprintPure)
+  bool GetPackageCollected() const;
+
+private:
+  float LocalTimer = 45;
+  bool isPackageCollected;
+  bool LocalPackagedDelivered = false;
+};
